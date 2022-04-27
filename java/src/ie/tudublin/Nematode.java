5265ededf8b3cc6bed7206997e59ac16b1c4a884
Blade1802 <C20396243@mytudublin.ie>
2022-04-27 18:25:22 +0100

    male done

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -74,6 +74,4 @@ public class Nematode {
         this.eyes = eyes;
     }
 
-    
-
 }
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -43,8 +43,66 @@ public class NematodeVisualiser extends PApplet
         }
 	}
 
+	int circleRadius = 40;
+	Nematode currentNematode = null;
 
 	public void draw()
-	{	
+	{
+		stroke(255);
+		strokeWeight(2);
+		noFill();	
+		int halfW = width/2;
+		int halfH = height/2;
+		int offset = 0;
+		currentNematode = nematodes.get(0);
+		point(halfW, halfH);
+		for(int i = 0; i < (currentNematode.getLength() + 1)/2; i++)
+		{
+			offset = circleRadius * i;
+
+			if(currentNematode.getLength() % 2 == 1)
+			{
+				// Segments
+				circle(halfW, halfH - offset, circleRadius);
+				circle(halfW, halfH + offset, circleRadius);
+				
+				// Limbs
+				if(currentNematode.isLimbs())
+				{
+					// Top segment limbs
+					line(halfW + circleRadius/2, halfH - offset, halfW + circleRadius, halfH - offset);
+					line(halfW - circleRadius/2, halfH - offset, halfW - circleRadius, halfH - offset);
+					// Bottom segment limbs
+					line(halfW + circleRadius/2, halfH + offset, halfW + circleRadius, halfH + offset);
+					line(halfW - circleRadius/2, halfH + offset, halfW - circleRadius, halfH + offset);
+				}
+			}
+			else
+			{
+				// Segments
+				circle(halfW, halfH + circleRadius/2 - offset, circleRadius);
+				circle(halfW, halfH - circleRadius/2 + offset, circleRadius);
+				// Top limbs
+				line(halfW + circleRadius/2, halfH + circleRadius/2 - offset, halfW + circleRadius, halfH + circleRadius/2 - offset);
+				line(halfW - circleRadius/2, halfH + circleRadius/2 - offset, halfW - circleRadius, halfH + circleRadius/2 - offset);
+				// Bottom limbs
+				line(halfW + circleRadius/2, halfH + circleRadius/2 + offset, halfW + circleRadius, halfH + circleRadius/2 + offset);
+				line(halfW - circleRadius/2, halfH + circleRadius/2 + offset, halfW - circleRadius, halfH + circleRadius/2 + offset);
+			}
+		}
+
+		String gender = currentNematode.getGender();
+		switch(gender)
+		{
+			case "m":
+			{
+				int y = halfH + (circleRadius/2) * currentNematode.getLength();
+				line(halfW, y, halfW, y + circleRadius/2);
+				y = y + 5 + circleRadius/2;
+				circle(halfW, y, 10);
+
+			}
+		}
+		
 	}
 }
